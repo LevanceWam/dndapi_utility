29ab437d3bbc1f7c0516b100dec6057923eb574f
LevanceWam <lkwamley@fullsail.edu>
2016-08-27 17:46:31 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+[ ![Codeship Status for kjrohr/dndapi_utility](https://codeship.com/projects/5bea2bc0-4bd1-0134-ca2b-625b98bf20ca/status?branch=master)](https://codeship.com/projects/170025)
+# dndapi Utility Tool
+
+## Installation
+
+``` javascript
+
+npm install dndapi_test --save-dev
+
+```
+
+## Usage
+
+### Require
+
+``` javascript
+
+const util = require('dndapi_test');
+
+```
+
+### .debug
+
+``` javascript
+
+util.debug('A meaningful title', obj);
+
+```
+
+### Expected output
+
+``` javascript
+
+====================================
+A meaningful title
+====================================
+obj
+Fri Aug 19 2016 21:00:29 GMT-0400 (EDT)
+
+```
---
+++ src/util.js
@@ -0,0 +1,18 @@
+// Exports the debug method of this file.
+exports.debug = (title, obj) => {
+  // Requires in the terminal-colors module.
+  require('terminal-colors');
+
+  // Get's the the date and time when used.
+  const ts = new Date();
+  // A seperator for formatting.
+  const seperator = '\n====================================\n';
+  // This is formatted and colored output for the terimal
+  const outputTerminal = seperator.grey + title.bold.yellow + seperator.grey + obj + '\n' + ts;
+
+  // If the DEBUG environmental variable exits
+  if (process.env.DEBUG) {
+      // Display the output on terminal
+      console.log(outputTerminal);
+    };
+};
